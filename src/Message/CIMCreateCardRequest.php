ffcdadf451720fedd168c59c533e35fec9b85c04
sachin <sachinp9797@gmail.com>
2024-02-06 16:58:21 +0530

    X2-1655: adding phone number to customer billing

--- src/Message/CIMCreateCardRequest.php
+++ src/Message/CIMCreateCardRequest.php
@@ -97,6 +97,7 @@ class CIMCreateCardRequest extends CIMAbstractRequest
             $req->state = $card->getBillingState();
             $req->zip = $card->getBillingPostcode();
             $req->country = $card->getBillingCountry();
+            $req->phoneNumber = $card->getBillingPhone();
 
             $defaultBillTo = $this->getParameter('defaultBillTo');
             if (is_array($defaultBillTo)) {
